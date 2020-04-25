c8bf886541eeb543ee408e5dd7fba5059eccc42b
sidmonta <sidmonta.15@gmail.com>
2020-04-25 19:07:48 +0200

    Add stopwords and metadata feature extract

--- src/Features.ts
+++ src/Features.ts
@@ -1,20 +1,30 @@
+import { removeStopwords } from 'stopword'
 
 export type FeaturesFun = (doc: unknown) => Map<string, number>
 
 export function getWords(doc: string): Map<string, number> {
-  const words = new Map<string, number>()
   const regex = new RegExp('[^A-Za-z-]')
 
-  const wordsInDoc = doc
-    .split(regex)
+  const wordsInDoc: [string, number][] = removeStopwords(doc.split(regex))
     .filter(word => word.length > 2 && word.length < 20)
-    .map(word => word.toLocaleLowerCase())
+    .map(word => [word.toLocaleLowerCase(), 1])
 
-  wordsInDoc.forEach(word => {
-    if (!words.has(word)) {
-      words.set(word, 1)
-    }
-  })
+  const words = new Map<string, number>(wordsInDoc)
+  return words
+}
+
+export function featureWthMetadata(
+  { metadata, content }: { metadata: { [a: string]: string }, content: string }
+) {
+  const words = new Map<string, number>()
+  const metadataToParse = Object.values(metadata).filter(met => !met.match(/[0-9-]+/))
+  for (const metadataValue of metadataToParse) {
+    words.set(metadataValue, 1)
+  }
+  const wordsOnDesc = getWords(content)
+  for (const wd of Array.from(wordsOnDesc.keys())) {
+    words.set(wd, 1)
+  }
 
   return words
 }
