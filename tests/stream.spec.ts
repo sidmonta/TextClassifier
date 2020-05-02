3e7631c6951764683c9a9b30f59094cca5de0ff2
sidmonta <sidmonta.15@gmail.com>
2020-05-02 16:46:54 +0200

    Script for tests

--- tests/stream.spec.ts
+++ tests/stream.spec.ts
@@ -17,33 +17,32 @@ import classify from '../src/stream'
   }
 })()
 
-// describe('Check stream good', async () => {
-//   const classify$ = classify<string>({
-//     dbPath: __dirname + '/test.db',
-//     algorithm: 'Fisher',
-//     featureFun: 'getWords'
-//   })
+describe('Check stream good', async () => {
+  const classify$ = classify<string>({
+    dbPath: __dirname + '/test.db',
+    algorithm: 'Fisher',
+    featureFun: 'getWords'
+  })
 
-//   classify$('id', 'quick rabbit').subscribe(data => {
-//     assert.deepEqual(data, ['id', 'good'])
-//   }).unsubscribe()
-// })
+  classify$('id', 'quick rabbit').subscribe(data => {
+    assert.deepEqual(data, ['id', 'good'])
+  }).unsubscribe()
+})
 
-// describe('Check stream bad', async () => {
-//   const classify$ = classify<string>({
-//     dbPath: __dirname + '/test.db',
-//     algorithm: 'Fisher',
-//     featureFun: 'getWords'
-//   })
+describe('Check stream bad', async () => {
+  const classify$ = classify<string>({
+    dbPath: __dirname + '/test.db',
+    algorithm: 'Fisher',
+    featureFun: 'getWords'
+  })
 
-//   classify$('idrrrr', 'quick money').subscribe(data => {
-//     assert.deepEqual(data, ['idrrrr', 'bad'])
-//   }).unsubscribe()
-// })
+  classify$('idrrrr', 'quick money').subscribe(data => {
+    assert.deepEqual(data, ['idrrrr', 'bad'])
+  }).unsubscribe()
+})
 
 
 describe('Check stream from stream', async () => {
-  console.log(__dirname + '/test.db')
 
   const classify$ = classify<string>({
     dbPath: __dirname + '/test.db',
@@ -51,17 +50,29 @@ describe('Check stream from stream', async () => {
     featureFun: 'getWords'
   })
 
+  const map = {
+    id: 'good',
+    id2: 'bad',
+    id3: 'good',
+    id4: 'bad',
+    id5: 'bad',
+    id6: 'good'
+  }
+
   of(
     ['id', 'quick rabbit'],
     ['id2', 'quick money'],
     ['id3', 'rabbit rabbit'],
     ['id4', 'money money'],
-    ['id5', 'money money'],
-    ['id6', 'money money']
+    ['id5', 'money'],
+    ['id6', 'rabbit']
   ).pipe(
     mergeMap(([id, item]) => {
-      console.log(id, item)
       return classify$(id, item)
     })
-  ).subscribe(console.log)
+  ).subscribe(result => {
+    const [id, res] = result
+
+    assert.equal(res, map[id])
+  })
 })
